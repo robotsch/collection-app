731485752522a8a57e29d39a24880adcfa0c7115
Ren <cassandra.chen@uoit.net>
2022-06-01 17:10:32 -0400

    :bento: add image and :truck: move code into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,12 @@
 // import { useState } from 'react'
-// import Input from './components/Input'
+import Input from './components/Input'
 import './App.css'
-import { Box } from '@mui/system'
-import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
 
 function App() {
 
   return (
     <div>
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-          'display': 'flex', 
-          'flexDirection': 'column',
-          'justifyContent': 'center',
-          'alignItems': 'center'
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Tell me what the date is"
-          variant="filled"
-        />
-        <TextField
-          id="filled-text1"
-          label="text1"
-          type="text"
-          variant="standard"
-        />
-        <TextField
-          id="filled-text2"
-          label="text2"
-          type="text"
-          variant="standard"
-        />
-        <TextField
-          id="filled-text3"
-          label="text3"
-          type="text"
-          variant="standard"
-        />
-        <Button
-          sx={{
-            'background-color': 'transparent',
-            'border': '2px solid black',
-          }}
-          type="file">
-          Upload Image
-        </Button>
-        <Button 
-          component="span"
-          id="submit"
-          label="submit"
-          type="submit"
-          variant="contained"
-          sx={{
-            'margin': '20px' 
-          }}
-        >
-          Submit
-        </Button>
-      </Box>
+      <Input/>
     </div>
   )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,79 @@
-import "./Input.scss"
+import { Box } from '@mui/system'
 import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
-export default function Input(props) {
+import './Input.scss'
 
+export default function Input() {
   return (
     <div>
-      <form>
-        <TextField id="outlined-basic" label={props.label} variant="outlined" />
-      </form>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          'display': 'flex', 
+          'flexDirection': 'column',
+          'justifyContent': 'center',
+          'alignItems': 'center'
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div id="logo-text">
+          <b>
+            <i>
+              Collection Application Logo Placeholder
+            </i>
+          </b>
+        </div>
+        <TextField
+          disabled
+          id="filled-disabled"
+          label="Disabled"
+          defaultValue="Tell me what the date is"
+          variant="filled"
+        />
+        <TextField
+          id="filled-text1"
+          label="text1"
+          type="text"
+          variant="standard"
+        />
+        <TextField
+          id="filled-text2"
+          label="text2"
+          type="text"
+          variant="standard"
+        />
+        <TextField
+          id="filled-text3"
+          label="text3"
+          type="text"
+          variant="standard"
+        />
+        <Button
+          id="upload-image"
+          sx={{
+            'background-color': 'transparent',
+            'border': '2px solid black',
+            'margin-top': '17px'
+          }}
+          type="file">
+          Upload Image
+        </Button>
+        <Button 
+          component="span"
+          id="submit"
+          label="submit"
+          type="submit"
+          variant="contained"
+          sx={{
+            'margin': '20px' 
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </div>
-  );
+  )
 }
