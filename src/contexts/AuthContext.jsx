b1a53e712dfd52463b05b2974bfcd0f155a47a94
robotsh <christianhumble22@gmail.com>
2022-06-02 11:38:19 -0400

    :recycle: Refactor auth promises

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,6 @@
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import React, { useContext, useState } from 'react'
-import { auth, signOut } from '../firebase'
+import { signInWithEmailAndPassword, signOut  } from 'firebase/auth'
+import React, { useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 const AuthContext = React.createContext()
 
@@ -10,44 +10,32 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState()
-  const [error, setError] = useState()
 
-  const signUp = (email, pw) => {
-    createUserWithEmailAndPAssword(auth, email, pw)
-      .then((userCredential) => {
-        setUser(userCredential.user)
-      })
-      .catch((err) => {
-        setError(err.message)
-      })
+  const signup = (email, pw) => {
+    return auth.createUserWithEmailAndPAssword(email, pw)
   }
 
-  const signIn = (email, pw) => {
-    signInWithEmailAndPassword(auth, email, pw)
-      .then((userCredential) => {
-        setUser(userCredential.user)
-      })
-      .catch((err) => {
-        setError(err.message)
-      })
+  const signin = (email, pw) => {
+    return auth.signInWithEmailAndPassword(email, pw)
   }
 
   const logout = () => {
-    signOut(auth)
-      .then(() => {
-        setUser('')
-      })
-      .catch((err) => {
-        setError(err.message)
-      })
+    return signOut(auth)
   }
 
-  auth.onAuthStateChanged(user => {
-    setUser(user)
-  })
+  useEffect(() => {
+    const unsub = auth.onAuthStateChanged(user => {
+      setUser(user)
+    })
+  
+    return unsub
+  },[])
 
   const value = {
-    user
+    user,
+    signup,
+    signin,
+    logout
   }
 
   return (
