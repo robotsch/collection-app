ca8343d0900b56d8161a19d832e2ca1998f5d1cf
Ren <cassandra.chen@uoit.net>
2022-06-06 18:49:43 -0400

    :sparkles: :construction: The start of the Loading component!

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import "./Loading.scss"
+
+export default function Loading(){
+  return(
+    <div className="spinner-container">
+      <div className="loading-spinner">
+      </div>
+    </div>
+  )
+}
