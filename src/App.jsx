76574d6055eec5371ce2ffdba4b7ee9e0f18eb5b
robotsh <christianhumble22@gmail.com>
2022-06-08 19:12:19 -0400

    :sparkle: Add protected route functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 
 import SignIn from "./components/SignIn"
 import SignUp from "./components/SignUp";
+import HomeLayout from "./components/HomeLayout";
+import ProtectedLayout from "./components/ProtectedLayout"
+
 import { AuthProvider } from "./contexts/AuthContext";
 
 import { Route, Routes } from "react-router-dom";
@@ -11,9 +14,13 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/form" element={<Input/>} />
+        <Route element={<HomeLayout />}>
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+        <Route element={<ProtectedLayout />}>
+          <Route path="/form" element={<Input/>} />
+        </Route>
       </Routes>
     </AuthProvider>
   )
